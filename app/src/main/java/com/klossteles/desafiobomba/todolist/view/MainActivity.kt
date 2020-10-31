8481dac255013e69b78c784929d953d3f1f16b9f
klossteles <klossteles@gmail.com>
2020-10-30 21:04:47 -0300

    Add fragment to main activity

--- app/src/main/java/com/klossteles/desafiobomba/todolist/view/MainActivity.kt
+++ app/src/main/java/com/klossteles/desafiobomba/todolist/view/MainActivity.kt
@@ -2,11 +2,21 @@ package com.klossteles.desafiobomba.todolist.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
 import com.klossteles.desafiobomba.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var manager: FragmentManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        manager = supportFragmentManager
+
+        val transaction = manager.beginTransaction()
+        transaction.replace(R.id.fragmentContainer, TodoListFragment())
+        transaction.commit()
+
     }
 }
